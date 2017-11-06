05091f3497cdcc8a0383fcacdf83c02947cc251c
Scott Idler <scott.a.idler@gmail.com>
2017-11-06 13:12:08 -0800

    added stubbed autodomain.py

---
+++ autodomain.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+sys.dont_write_bytecode = True
+
+print('name: ', __file__)
+print('args: ', ' '.join(sys.argv[1:]))
+
+
+SCRIPTNAME = os.path.splitext(__file__)[0]
+
+from ruamel import yaml
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description=__doc__,
+        formatter_class=RawDescriptionHelpFormatter,
+        add_help=False)
+    parser.add_argument(
+        '--config',
+        metavar='FILEPATH',
+        default='%(SCRIPTNAME)s.yml' % globals(),
+        help='default="%(default)s"; config filepath')
+    ns, rem = parser.parse_known_args()
+    try:
+        config = yaml.safe_load(open(ns.config))
+    except FileNotFoundError as er:
+        config = dict()
+    parser = ArgumentParser(
+        parents=[parser])
+    parser.set_defaults(**config)
+    parser.add_argument(
+        '--firstname',
+        help='first name')
+    parser.add_argument(
+        '--lastname',
+        help='last name')
+    parser.add_argument(
+        '--age',
+        help='age')
+    ns = parser.parse_args(rem)
+    print(ns)
