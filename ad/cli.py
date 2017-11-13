35877eb131d02b5bb44aaa99fc19e1f8af1af0c7
Scott Idler <scott.a.idler@gmail.com>
2017-11-13 08:51:57 -0800

    added stubs for subcommands ls, create and change

--- ad/cli.py
+++ ad/cli.py
@@ -16,11 +16,38 @@ FILE = os.path.realpath(__file__)
 SCRIPTNAME = os.path.splitext(FILE)[0]
 SCRIPTPATH = os.path.dirname(os.path.abspath(FILE))
 
+def command_subparsers(parser):
+    subparsers = parser.add_subparsers(
+        dest='command',
+        title='commands',
+        description='choose a command to run')
+    return subparsers
+
+def ls_parser(subparsers):
+    parser = subparsers.add_parser('ls')
+    parser.add_argument(
+        '--ls-arg',
+        help='ls arg')
+    return parser
+
+def create_parser(subparsers):
+    parser = subparsers.add_parser('create')
+    parser.add_argument(
+        '--create-arg',
+        help='create arg')
+
+def change_parser(subparsers):
+    parser = subparsers.add_parser('change')
+    parser.add_argument(
+        '--change-arg',
+        help='change arg')
+
 def main(args):
-    parser = ArgumentParser(
-        description=__doc__,
-        formatter_class=RawDescriptionHelpFormatter,
-        add_help=False)
+#    parser = ArgumentParser(
+#        description=__doc__,
+#        formatter_class=RawDescriptionHelpFormatter,
+#        add_help=False)
+    parser = ArgumentParser(add_help=False)
     parser.add_argument(
         '--config',
         metavar='FILEPATH',
@@ -32,7 +59,10 @@ def main(args):
     except FileNotFoundError as er:
         config = dict()
     parser = ArgumentParser(
-        parents=[parser])
+        parents=[parser],
+        description=__doc__,
+        formatter_class=RawDescriptionHelpFormatter)
+
     parser.set_defaults(**config)
     parser.add_argument(
         '--firstname',
@@ -43,9 +73,15 @@ def main(args):
     parser.add_argument(
         '--age',
         help='default="%(default)s"; age')
+
+    subparsers = command_subparsers(parser)
+    ls_parser(subparsers)
+    create_parser(subparsers)
+    change_parser(subparsers)
+
     ns = parser.parse_args(rem)
     print(ns)
     return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv[1:]))
